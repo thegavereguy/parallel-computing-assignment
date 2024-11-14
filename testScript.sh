4ec50ddef00a9df557c316a45b81291eb7dd73e2
thegavereguy <dallacia.enrico@protonmail.com>
2024-11-14 16:17:27 +0100

    changed test script to differentiate tests with different threads number

--- testScript.sh
+++ testScript.sh
@@ -2,8 +2,18 @@
 
 array=("mat_trans_par" "mat_trans_par_col" "mat_trans_seq");
 
+# check if the OMP_NUM_THREADS is set
+# if not set, set it to 1
+if [ -z "$OMP_NUM_THREADS" ]; then
+	export OMP_NUM_THREADS=1;
+fi
+
+if [ -z "$BENCHMARK_SAMPLES" ]; then
+	export BENCHMARK_SAMPLES=10;
+fi
+
 for i in ${array[@]}; do
-		eval "./tests/tests" \"[$i]\" "-r csv" "--benchmark-samples=10"> results/"$i.txt";
+		eval "./build/tests/tests" \"[$i]\" "-r csv" "--benchmark-samples=$BENCHMARK_SAMPLES"> results/"$i"".$OMP_NUM_THREADS.txt";
 done
 
 
