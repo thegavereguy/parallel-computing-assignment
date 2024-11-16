8adfe9d7eca802c8e9ba340981cdc9a3ce6a906d
thegavereguy <dallacia.enrico@protonmail.com>
2024-11-16 16:15:47 +0100

    cleanup

--- apps/app.cpp
+++ apps/app.cpp
@@ -5,17 +5,6 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-  // int a = 2;
-  // int b = 4;
-  //
-  // std::cout << "Sum of " << a << " and " << b << " is " << sum(a, b)
-  //           << std::endl;
-  // std::cout << "Product of " << a << " and " << b << " is " << mul(a, b)
-  //           << std::endl;
-  //
-  // parallel_test();
-  // std::cout << "Pi: " << calculate_pi_parallel(10000000) << std::endl;
-  //
   int n   = 100;
   int** A = new int*[n];
   int** B = new int*[n];
@@ -27,11 +16,6 @@ int main(int argc, char** argv) {
     }
   }
   transpose_sequential(n, A, B);
-  // for (int i = 0; i < n; i++) {
-  //   for (int j = 0; j < n; j++) {
-  //     std::cout << B[i][j] << " ";
-  //   }
-  // }
   deallocate(A, n);
   deallocate(B, n);
 
