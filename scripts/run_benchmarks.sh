15cd61df06c97c8e68a2a3f95672bdb11010dd79
thegavereguy <dallacia.enrico@protonmail.com>
2024-11-29 14:57:22 +0100

    update to the scripts

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -50,6 +50,6 @@ fi
 echo "Running $BENCHMARK_SAMPLES samples per benchmark with $BENCHMARK_CONFIDENCE_INTERVAL confidence interval";
 for i in ${array[@]}; do
 		echo "Running benchmark for [$i] with $OMP_NUM_THREADS threads";
-		eval "./build/tests/benchmarks" \"[$i]\" "-r csv" "--benchmark-samples=$BENCHMARK_SAMPLES" "--benchmark-confidence-interval=$BENCHMARK_CONFIDENCE_INTERVAL" > results/benchmarks/"$i"".$OMP_NUM_THREADS.csv";
+		eval "./build/tests/benchmarks" \"[$i]\" "-r csv" "--benchmark-samples=$BENCHMARK_SAMPLES" "--benchmark-confidence-interval=$BENCHMARK_CONFIDENCE_INTERVAL" > results/benchmarks/"$i"".$BUILD_OPTIMIZED.$OMP_NUM_THREADS.csv";
 done
 
